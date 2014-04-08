dbea8fcc0915b258ec0289ad4aacd2489ef15234
alaalkhaldi <alkalidi@gmail.com>
2014-04-08 00:10:49 -0500

    update

--- src/java/org/apache/cassandra/db/DefsTable.java
+++ src/java/org/apache/cassandra/db/DefsTable.java
@@ -342,31 +342,31 @@ public class DefsTable
 
         for (RowMutation mutation : mutations){
             
-//        	if(mutation.getTable().equals(Metadata.MetaData_KS)){
-//        		 ColumnFamily cf = mutation.getColumnFamily(Schema.instance.getCFMetaData(Metadata.MetaData_KS, Metadata.MetadataLog_CF).cfId);           
-//                 if(cf != null ){
-//                	// check if insertion
-//                 	if(cf.getSortedColumns().size() != 0){                 		
-//                 		// check if the metadata log insertion should be applied
-//                 		// if no entry is found in the system_metadata.registry table. don't apply the mutation.
-//     	            	Iterator<IColumn> itr = cf.getSortedColumns().iterator();
-//     	            	String dataTag = itr.next().getString(cf.getComparator());
-//     	            	
-//     	            	int colNameBoundary = dataTag.indexOf("false");
-//     	            	if(colNameBoundary == -1) 
-//     	            		colNameBoundary = dataTag.indexOf("true");
-//     	            	
-//     	            	dataTag = dataTag.substring(0, colNameBoundary-1);
-//     	            	dataTag = dataTag.substring(dataTag.indexOf(":")+1);
-//     	            	dataTag = dataTag.substring(dataTag.indexOf(":")+1, dataTag.lastIndexOf(":"));
-//     	            	
-//     	            	String exists = MetadataRegistry.instance.query(new String(mutation.key().array()), dataTag);
-//     	            	if(exists == null){
-//     	            		continue;
-//     	            	}
-//                 	}
-//                 }    
-//        	}
+        	if(mutation.getTable().equals(Metadata.MetaData_KS)){
+        		 ColumnFamily cf = mutation.getColumnFamily(Schema.instance.getCFMetaData(Metadata.MetaData_KS, Metadata.MetadataLog_CF).cfId);           
+                 if(cf != null ){
+                	// check if insertion
+                 	if(cf.getSortedColumns().size() != 0){                 		
+                 		// check if the metadata log insertion should be applied
+                 		// if no entry is found in the system_metadata.registry table. don't apply the mutation.
+     	            	Iterator<IColumn> itr = cf.getSortedColumns().iterator();
+     	            	String dataTag = itr.next().getString(cf.getComparator());
+     	            	
+     	            	int colNameBoundary = dataTag.indexOf("false");
+     	            	if(colNameBoundary == -1) 
+     	            		colNameBoundary = dataTag.indexOf("true");
+     	            	
+     	            	dataTag = dataTag.substring(0, colNameBoundary-1);
+     	            	dataTag = dataTag.substring(dataTag.indexOf(":")+1);
+     	            	dataTag = dataTag.substring(dataTag.indexOf(":")+1, dataTag.lastIndexOf(":"));
+     	            	
+     	            	String exists = MetadataRegistry.instance.query(new String(mutation.key().array()), dataTag);
+     	            	if(exists == null){
+     	            		continue;
+     	            	}
+                 	}
+                 }    
+        	}
         	
         	// Apply the mutation in all other cases
         	mutation.apply();
